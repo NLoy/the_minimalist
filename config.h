76c4796bba449c29822b69ed0f2917ae0e476da2
NLoy <50891875+NLoy@users.noreply.github.com>
2022-10-05 14:17:05 -0500

    Initial attempt
    Does not compile

---
+++ config.h
@@ -0,0 +1,148 @@
+// Copyright 2022 Noah (@Noah)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+
+
+
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0xFADE
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    qmkbuilder
+#define PRODUCT         keyboard
+
+
+
+#define CAP_PINS                      \
+    {                                 \
+            LINE_PIN11, /* CAP_BLU */ \
+            LINE_PIN12, /* CAP_RED */ \
+            LINE_PIN24, /* CAP_GRN */ \
+            LINE_PIN25, /* CAP_YEL */ \
+            LINE_PIN26, /* CAP_WHI */ \
+    }
+
+
+/* key matrix size */
+#define MATRIX_ROWS 9
+#define MATRIX_COLS 25
+
+#define MATRIX_ROW_PINS             \
+    {                               \
+            LINE_PIN9, /* ROW_1 */  \
+            LINE_PIN8, /* ROW_2 */  \
+            LINE_PIN7, /* ROW_3 */  \
+            LINE_PIN6, /* ROW_4 */  \
+            LINE_PIN5, /* ROW_5 */  \
+            LINE_PIN4, /* ROW_6 */  \
+            LINE_PIN3, /* ROW_7 */  \
+            LINE_PIN2, /* ROW_8 */  \
+            LINE_PIN1  /* ROW_9 */  \
+    }
+
+#define MATRIX_COL_PINS             \
+    {                               \
+            LINE_PIN27, /* COL_A */ \
+            LINE_PIN28, /* COL_B */ \
+            LINE_PIN29, /* COL_C */ \
+            LINE_PIN30, /* COL_D */ \
+            LINE_PIN31, /* COL_E */ \
+            LINE_PIN32, /* COL_F */ \
+            LINE_PIN33, /* COL_G */ \
+            LINE_PIN34, /* COL_H */ \
+            LINE_PIN35, /* COL_I */ \
+            LINE_PIN36, /* COL_J */ \
+            LINE_PIN37, /* COL_K */ \
+            LINE_PIN38, /* COL_L */ \
+            LINE_PIN39, /* COL_M */ \
+            LINE_PIN40, /* COL_N */ \
+            LINE_PIN41, /* COL_O */ \
+            LINE_PIN14, /* COL_P */ \
+            LINE_PIN15, /* COL_Q */ \
+            LINE_PIN16, /* COL_R */ \
+            LINE_PIN17, /* COL_S */ \
+            LINE_PIN18, /* COL_T */ \
+            LINE_PIN19, /* COL_U */ \
+            LINE_PIN20, /* COL_V */ \
+            LINE_PIN21, /* COL_W */ \
+            LINE_PIN22, /* COL_X */ \
+            LINE_PIN23  /* COL_Y */ \
+    }
+
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION ROW2COL
+
+/* Set 0 if debouncing isn't needed */
+/* Well-worn Cherry MX key switches can bounce for up to 20ms, despite the
+ * Cherry data sheet specifying 5ms. Because we use the sym_eager_pk debounce
+ * algorithm, this debounce latency only affects key releases (not key
+ * presses). */
+#undef DEBOUNCE
+#define DEBOUNCE 20
+
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
+
+
+#ifdef RGB_DI_PIN
+
+#define RGB_DI_PIN LINE_PIN0
+#define RGBLED_NUM 69
+#define RGBLIGHT_SLEEP //If defined, the RGB lighting will be switched off when the host goes to sleep
+#define RGBLIGHT_LAYERS_RETAIN_VAL //Allows RGB Layer brightness  to be bound to regular RGB brightness
+
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_LIMIT_VAL 200
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_RGB TEST
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_TWINKLE
+
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_MAX_LAYERS 8
+
+#endif
+
+#endif
---
+++ the_minimalist.h
@@ -0,0 +1,51 @@
+/* Copyright 2020 QMK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "quantum.h"
+
+
+// This a shortcut to help you visually see your layout.
+// The first section contains all of the arguments as on the physical keyboard
+// The second converts the arguments into the 2-D scanned array
+
+
+
+#define LAYOUT( \
+        KC_1A,   KC_1B,   KC_1C,           KC_1D,   KC_1E,   KC_1F,   KC_1G,   KC_1H,   KC_1I,   KC_1J,   KC_1K,   KC_1L,   KC_1M,   KC_1N,   KC_1O,   KC_1P,   KC_1Q,   KC_1R,           KC_1S,   KC_1T,   KC_1U,           KC_1V,   KC_1W,   KC_1X,   KC_1Y, \
+        KC_2A,   KC_2B,   KC_2C,           KC_2D,   KC_2E,   KC_2F,   KC_2G,   KC_2H,   KC_2I,   KC_2J,   KC_2K,   KC_2L,   KC_2M,   KC_2N,   KC_2O,   KC_2P,   KC_2Q,   KC_2R,           KC_2S,   KC_2T,   KC_2U,           KC_2V,   KC_2W,   KC_2X,   KC_2Y, \
+        KC_3A,   KC_3B,   KC_3C,           KC_3D,   KC_3E,   KC_3F,   KC_3G,   KC_3H,   KC_3I,   KC_3J,   KC_3K,   KC_3L,   KC_3M,   KC_3N,   KC_3O,   KC_3P,   KC_3Q,   KC_3R,           KC_3S,   KC_3T,   KC_3U,           KC_3V,   KC_3W,   KC_3X,   KC_3Y, \
+        KC_4A,   KC_4B,   KC_4C,           KC_4D,   KC_4E,   KC_4F,   KC_4G,   KC_4H,   KC_4I,   KC_4J,   KC_4K,   KC_4L,   KC_4M,   KC_4N,   KC_4O,   KC_4P,   KC_4Q,   KC_4R,           KC_4S,   KC_4T,   KC_4U,           KC_4V,   KC_4W,   KC_4X,   KC_4Y, \
+\
+        KC_5A,   KC_5B,   KC_5C,           KC_5D,   KC_5E,   KC_5F,   KC_5G,   KC_5H,   KC_5I,   KC_5J,   KC_5K,   KC_5L,   KC_5M,   KC_5N,   KC_5O,   KC_5P,            KC_5R,           KC_5S,   KC_5T,   KC_5U,           KC_5V,   KC_5W,   KC_5X,   KC_5Y, \
+        KC_6A,   KC_6B,   KC_6C,           KC_6D,   KC_6E,   KC_6F,   KC_6G,   KC_6H,   KC_6I,   KC_6J,   KC_6K,   KC_6L,   KC_6M,   KC_6N,   KC_6O,   KC_6P,            KC_6R,           KC_6S,   KC_6T,   KC_6U,           KC_6V,   KC_6W,   KC_6X,   KC_6Y, \
+        KC_7A,   KC_7B,   KC_7C,           KC_7D,   KC_7E,   KC_7F,   KC_7G,   KC_7H,   KC_7I,   KC_7J,   KC_7K,   KC_7L,   KC_7M,   KC_7N,   KC_7O,   KC_7P,                             KC_7S,   KC_7T,   KC_7U,           KC_7V,   KC_7W,   KC_7X,          \
+        KC_8A,   KC_8B,   KC_8C,           KC_8D,            KC_8F,   KC_8G,   KC_8H,   KC_8I,   KC_8J,   KC_8K,   KC_8L,   KC_8M,   KC_8N,   KC_8O,   KC_8P,                             KC_8S,   KC_8T,   KC_8U,           KC_8V,   KC_8W,   KC_8X,   KC_8Y, \
+        KC_9A,   KC_9B,   KC_9C,           KC_9D,   KC_9E,   KC_9F,                              KC_9J,                              KC_9N,   KC_9O,   KC_9P,            KC_9R,           KC_9S,   KC_9T,   KC_9U,           KC_9V,            KC_9X           \
+    ) \
+    { \
+       KC_1A,   KC_1B,   KC_1C,           KC_1D,   KC_1E,   KC_1F,   KC_1G,   KC_1H,   KC_1I,   KC_1J,   KC_1K,   KC_1L,   KC_1M,   KC_1N,   KC_1O,   KC_1P,   KC_1Q,   KC_1R,           KC_1S,   KC_1T,   KC_1U,           KC_1V,   KC_1W,   KC_1X,   KC_1Y, \
+       KC_2A,   KC_2B,   KC_2C,           KC_2D,   KC_2E,   KC_2F,   KC_2G,   KC_2H,   KC_2I,   KC_2J,   KC_2K,   KC_2L,   KC_2M,   KC_2N,   KC_2O,   KC_2P,   KC_2Q,   KC_2R,           KC_2S,   KC_2T,   KC_2U,           KC_2V,   KC_2W,   KC_2X,   KC_2Y, \
+       KC_3A,   KC_3B,   KC_3C,           KC_3D,   KC_3E,   KC_3F,   KC_3G,   KC_3H,   KC_3I,   KC_3J,   KC_3K,   KC_3L,   KC_3M,   KC_3N,   KC_3O,   KC_3P,   KC_3Q,   KC_3R,           KC_3S,   KC_3T,   KC_3U,           KC_3V,   KC_3W,   KC_3X,   KC_3Y, \
+       KC_4A,   KC_4B,   KC_4C,           KC_4D,   KC_4E,   KC_4F,   KC_4G,   KC_4H,   KC_4I,   KC_4J,   KC_4K,   KC_4L,   KC_4M,   KC_4N,   KC_4O,   KC_4P,   KC_4Q,   KC_4R,           KC_4S,   KC_4T,   KC_4U,           KC_4V,   KC_4W,   KC_4X,   KC_4Y, \
+\
+       KC_5A,   KC_5B,   KC_5C,           KC_5D,   KC_5E,   KC_5F,   KC_5G,   KC_5H,   KC_5I,   KC_5J,   KC_5K,   KC_5L,   KC_5M,   KC_5N,   KC_5O,   KC_5P, _______,   KC_5R,           KC_5S,   KC_5T,   KC_5U,           KC_5V,   KC_5W,   KC_5X,   KC_5Y, \
+       KC_6A,   KC_6B,   KC_6C,           KC_6D,   KC_6E,   KC_6F,   KC_6G,   KC_6H,   KC_6I,   KC_6J,   KC_6K,   KC_6L,   KC_6M,   KC_6N,   KC_6O,   KC_6P, _______,   KC_6R,           KC_6S,   KC_6T,   KC_6U,           KC_6V,   KC_6W,   KC_6X,   KC_6Y, \
+       KC_7A,   KC_7B,   KC_7C,           KC_7D,   KC_7E,   KC_7F,   KC_7G,   KC_7H,   KC_7I,   KC_7J,   KC_7K,   KC_7L,   KC_7M,   KC_7N,   KC_7O,   KC_7P, _______, _______,           KC_7S,   KC_7T,   KC_7U,           KC_7V,   KC_7W,   KC_7X, _______, \
+       KC_8A,   KC_8B,   KC_8C,           KC_8D, _______,   KC_8F,   KC_8G,   KC_8H,   KC_8I,   KC_8J,   KC_8K,   KC_8L,   KC_8M,   KC_8N,   KC_8O,   KC_8P, _______, _______,           KC_8S,   KC_8T,   KC_8U,           KC_8V,   KC_8W,   KC_8X,   KC_8Y, \
+       KC_9A,   KC_9B,   KC_9C,           KC_9D,   KC_9E,   KC_9F, _______, _______, _______,   KC_9J, _______, _______, _______,   KC_9N,   KC_9O,   KC_9P, _______,   KC_9R,           KC_9S,   KC_9T,   KC_9U,           KC_9V, _______,   KC_9X, _______, \
+    };
